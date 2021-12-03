7d42521a431ff1f1aebd5915773f592a941bc018
Kevin Pavao <kpavao84@gmail.com>
2021-12-03 15:56:47 -0600

    day03 cleanup

--- src/advent_of_code_2021/day03.clj
+++ src/advent_of_code_2021/day03.clj
@@ -2,33 +2,32 @@
   (:require [clojure.edn :as edn]
             [advent-of-code-2021.util :as util]))
 
-(defn count-bits
-  [bits bit]
-  (count (filter #(= bit %) bits)))
-
 (defn flip-bits
   [bits]
   (apply str (map #(if (= \0 %) \1 \0) bits)))
 
 (defn most-common
   [bits]
-  (if (> (count-bits bits \0) (count-bits bits \1))
-    0 1))
+  (let [{zeroes \0 ones \1} (frequencies bits)]
+    (if (> zeroes ones) 0 1)))
 
-(defn bits->int
-  [bits]
-  (edn/read-string (str "2r" bits)))
-
-(defn part1 
+(defn get-rates
   [input]
-  (let [gammas (for [n (range (count (first input)))] (apply str (map #(nth % n) input)))
-        gamma (apply str (map most-common gammas))]
-    {:gamma (bits->int gamma)
-     :epsilon (bits->int (flip-bits gamma))}))
+  (let [diagnostics (for [n (range (count (first input)))] (apply str (map #(nth % n) input)))
+        gamma (apply str (map most-common diagnostics))
+        epsilon (flip-bits diagnostics)]
+    {:gamma (Long/parseLong gamma 2)
+     :epsilon (Long/parseLong epsilon 2)}))
+
+(defn part1
+  [input]
+  (let [rates (get-rates input)]
+    (* (:epsilon rates) (:gamma rates))))
 
 (comment
   (def sample (util/read-input "day03sample.txt"))
-  (part1 sample)
+  (= 198 (part1 sample))
+  (= 1131506 (part1 (util/read-input "day03.txt")))
 
   (apply str (map #(first %) sample))
   (for [n (range (count (first sample)))] (apply str (map #(nth % n) sample)))
