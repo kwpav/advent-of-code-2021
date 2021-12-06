e445b60378493b3fb64753457d2bfe14b6109a45
Kevin Pavao <kpavao84@gmail.com>
2021-12-05 23:10:55 -0600

    initial day 3 solutions

--- src/advent_of_code_2021/day03.clj
+++ src/advent_of_code_2021/day03.clj
@@ -1,37 +1,75 @@
 (ns advent-of-code-2021.day03
-  (:require [clojure.edn :as edn]
-            [advent-of-code-2021.util :as util]))
+  (:require [advent-of-code-2021.util :as util]))
+
+(defn flip-bit
+  [bit]
+  (if (= \0 bit) \1 \0))
 
 (defn flip-bits
   [bits]
-  (apply str (map #(if (= \0 %) \1 \0) bits)))
+  (apply str (map #(flip-bit %) bits)))
 
 (defn most-common
   [bits]
   (let [{zeroes \0 ones \1} (frequencies bits)]
-    (if (> zeroes ones) 0 1)))
+    (cond
+      (nil? zeroes) \1
+      (nil? ones) \0
+      (> zeroes ones) \0
+      :else \1)))
+
+(defn parse-diagnostics
+  [diagnostics]
+  (for [n (range (count (first diagnostics)))]
+    (apply str (map #(nth % n) diagnostics))))
 
 (defn get-rates
-  [input]
-  (let [diagnostics (for [n (range (count (first input)))] (apply str (map #(nth % n) input)))
-        gamma (apply str (map most-common diagnostics))
-        epsilon (flip-bits diagnostics)]
+  [diagnostics]
+  (let [rate-diagnostics (parse-diagnostics diagnostics)
+        gamma (->> rate-diagnostics
+                   (map most-common)
+                   (apply str))
+        epsilon (flip-bits gamma)]
     {:gamma (Long/parseLong gamma 2)
      :epsilon (Long/parseLong epsilon 2)}))
 
-(defn part1
+;; part1
+(defn power-consumption
   [input]
   (let [rates (get-rates input)]
     (* (:epsilon rates) (:gamma rates))))
 
+;; part2
+(defn oxygen-bit-criteria 
+  [diagnostics n]
+  (let [parsed (parse-diagnostics diagnostics)]
+    (vec (filter #(= (most-common (nth parsed n)) (nth % n)) diagnostics))))
+
+(defn co2-bit-criteria
+  [diagnostics n]
+  (let [parsed (parse-diagnostics diagnostics)]
+    (vec (filter #(= (flip-bit (most-common (nth parsed n))) (nth % n)) diagnostics))))
+
+(defn get-system-rating
+  [diagnostics criteria]
+  (loop [diagnostics diagnostics n 0]
+    (if (= 1 (count diagnostics))
+      (first diagnostics)
+      (recur (criteria diagnostics n) (inc n)))))
+
+(defn life-support-rating
+  [input]
+  (* (Long/parseLong (get-system-rating input oxygen-bit-criteria) 2)
+     (Long/parseLong (get-system-rating input co2-bit-criteria) 2)))
+
 (comment
   (def sample (util/read-input "day03sample.txt"))
-  (= 198 (part1 sample))
-  (= 1131506 (part1 (util/read-input "day03.txt")))
 
-  (apply str (map #(first %) sample))
-  (for [n (range (count (first sample)))] (apply str (map #(nth % n) sample)))
+  ;; part1
+  (= 198 (power-consumption sample))
+  (= 1131506 (power-consumption (util/read-input "day03.txt")))
 
-  (partition 1 1 sample)
-  (take (count (first sample)) (iterate first sample))
+  ;; part2
+  (= 230 (life-support-rating sample))
+  (= 7863147 (life-support-rating (util/read-input "day03.txt")))
   ,)
