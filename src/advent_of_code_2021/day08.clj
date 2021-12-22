70b5f3752b66a599178113ea20d80c4e5a0ecf25
Kevin Pavao <kpavao84@gmail.com>
2021-12-21 21:05:21 -0600

    day08 wip

--- src/advent_of_code_2021/day08.clj
+++ src/advent_of_code_2021/day08.clj
@@ -1,7 +1,8 @@
 (ns advent-of-code-2021.day08
-  (:require [advent-of-code-2021.util :as util]
-            [clojure.string :as str]
-            [clojure.set :as set]))
+  (:require
+   [advent-of-code-2021.util :as util]
+   [clojure.set :as set]
+   [clojure.string :as str]))
 
 (defn digit-set
   [digit]
@@ -72,6 +73,18 @@
   [middle one four]
   (set/difference four (set/union one middle)))
 
+(defn get-three
+  [one five-segments]
+  (filter #(= 3 (count (set/difference % one))) five-segments))
+
+(defn get-five
+  [four five-segments]
+  (filter #(= 2 (count (set/difference % four))) five-segments))
+
+(defn get-two
+  [four five-segments]
+  (filter #(= 3 (count (set/difference % four))) five-segments))
+
 (defn get-nine
   [top-right six-segments]
   (filter (fn [s] (some #(= top-right %) s)) six-segments))
@@ -80,7 +93,7 @@
 [segment-length vs]
 (filter #(= (count %) segment-length) vs))
 
-(defn get-all-segments
+(defn map-all-digits
   "take parsed input and make digits"
   [input]
   (let [all-digits (into (:signal-pattern input) (:output-value input))
@@ -90,15 +103,68 @@
         [eight] (find-segments 7 all-digits)
         six-segments (find-segments 6 all-digits)
         five-segments (find-segments 5 all-digits)
-        top (get-top seven one)
-        bottom-left (get-bottom-left one six-segments)
-        top-right (get-top-right one six-segments)
-        bottom-right (get-bottom-right one top-right)
-        bottom (get-bottom top bottom-left four five-segments)
-        middle (get-middle top top-right bottom-left bottom five-segments)
-        top-left (get-top-left middle one four)]
-    (zipmap (mapv first [top top-left top-right middle bottom bottom-left bottom-right])
-            [:top :top-left :top-right :middle :bottom :bottom-left :bottom-right])))
+        [three] (get-three one five-segments)
+        [five] (get-five four (filter #(not= three %) five-segments))
+        [two] (filter #(and (not= three %) (not= five %)) five-segments)
+        [nine] (filter #(= 1 (count (set/difference % three))) six-segments)
+        [six] (filter #(= 5 (count (set/difference % one))) six-segments)
+        [zero] (filter #(and (not= nine %) (not= six %)) six-segments)
+        ;; top (get-top seven one)
+        ;; bottom-left (get-bottom-left one six-segments)
+        ;; top-right (get-top-right one six-segments)
+        ;; bottom-right (get-bottom-right one top-right)
+        ;; bottom (get-bottom top bottom-left four five-segments)
+        ;; middle (get-middle top top-right bottom-left bottom five-segments)
+        ;; top-left (get-top-left middle one four)
+        ]
+    ;; (zipmap (mapv first [top top-left top-right middle bottom bottom-left bottom-right])
+    ;;         [:top :top-left :top-right :middle :bottom :bottom-left :bottom-right])
+    {one 1
+     two 2
+     three 3
+     four 4
+     five 5
+     six 6
+     seven 7
+     eight 8
+     nine 9
+     zero 0}))
+
+(defn decode-output
+  [digit-map output]
+  (map #(get digit-map %) output))
+
+(defn decode-outputs
+  [input]
+  (let [digit-maps (map (fn [s] {(map-all-digits s) (:output-value s)}) input)]
+    digit-maps))
+
+(def segment-numbers
+  {#{:top :top-left :top-right :bottom-left :bottom-right :bottom} 0
+   #{:top-right :bottom-right} 1
+   #{:top :top-right :middle :bottom-left :bottom} 2
+   #{:top :top-right :middle :bottom-right :bottom} 3
+   #{:top-left :top-right :middle :bottom-right} 4
+   #{:top :top-left :middle :bottom-right :bottom} 5
+   #{:top :top-left :middle :bottom-left :bottom-right :bottom} 6
+   #{:top :top-right :bottom-left} 7
+   #{:top :top-left :top-right :middle :bottom-left :bottom-right :bottom} 8
+   #{:top :top-left :top-right :middle :bottom-right :bottom} 9})
+
+(defn digits->numbers
+  [digits]
+  (let [segments (map map-all-digits digits)]
+    (println (get (first segments) "d"))
+    (->> digits
+         (map :output-value)
+         first
+         second
+         ;; first
+         ;; (map #(get (first segments) %))
+         ;; (filter #(not (= nil %)))
+         ;; (into #{})
+         ;; (get segment-numbers)
+         )))
 
 (comment
   (def sample "day08sample.txt")
@@ -106,6 +172,8 @@
   ;; part 1
   (= 26 (count-easy-digits (parse-input sample)))
   (= 488 (count-easy-digits (parse-input "day08.txt")))
+
+  (digits->numbers (parse-input sample))
 
   ;; ultimate goal
   {\d :top
@@ -142,5 +210,9 @@
          (map #(str/split % #" \| "))
          (map (fn [[signal output]] {:signal-pattern (parse-digits signal) :output-value (parse-digits output)}))))
 
-  (get-all-segments (first simple-sample))
+  (map-all-digits (first simple-sample))
+  (decode-output (map-all-digits (first simple-sample)) (:output-value (first simple-sample)))
+
+  (decode-outputs (parse-input sample))
+
   ,)
